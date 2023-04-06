86ce94eceeb9aaa02113dbce1ac5069ec0471d53
ashle <amireles1334@yahoo.com>
2023-04-06 13:12:46 -0500

    fixed index.js main code

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import logger from './utilities/logger'
 import router from './routes'
 import {notFound, errorHandler} from './utilities/errors'
 
-const port = parseInt(process.env.PORT, 10) || 3000
+const port = Number(process.env.PORT)
 
 const app = express()
 
@@ -17,11 +17,11 @@ app.use(morgan(process.env.MORGAN_LOG))
 app.use(cors({origin: process.env.ORIGIN}))
 app.use(helmet())
 
-app.use(router)
+app.use('/', router)
 
 app.use(notFound)
 app.use(errorHandler)
 
-app.listen(port, () =>
-  logger.info('Application started at http://localhost:${process.env.PORT}'),
-)
+app.listen(port, () => {
+  logger.info('Server is running on port ${port}')
+})
