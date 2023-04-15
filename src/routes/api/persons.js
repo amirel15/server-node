904be776e42294be9a77ff8955a4366263c39047
ashle <amireles1334@yahoo.com>
2023-04-14 21:20:42 -0500

    added companies

--- src/routes/api/persons.js
+++ src/routes/api/persons.js
@@ -1,50 +1,50 @@
-import {Router} from 'express'
+import { Router } from 'express'
 
 import {
-    getPersons,
-    getPerson,
-    createPerson, 
-    updatePerson, 
-    deletePerson
+  getPersons,
+  getPerson,
+  createPerson,
+  updatePerson,
+  deletePerson,
 } from '../../models/persons'
 
 const router = Router()
 
 router.get('/', (req, res) => {
-    const persons = getPersons()
-    res.send(persons)
+  const persons = getPersons()
+  res.send(persons)
 })
 
 router.get('/:id', (req, res) => {
-    const person = getPerson(req.params.id)
-    if(person) {
-        res.send(person)
-    }
-    res.status(404).send({msg: 'Person Not Found'})
+  const person = getPerson(req.params.id)
+  if (person) {
+    res.send(person)
+  }
+  res.status(404).send({ msg: 'Person Not Found' })
 })
 
 router.post('/', (req, res) => {
-    const newPerson = createPerson(req.body)
-    if(newPerson){
-        res.status(201).send(newPerson)
-    }
-    res.status(400).send({msg: 'Bad Request'})
+  const newPerson = createPerson(req.body)
+  if (newPerson) {
+    res.status(201).send(newPerson)
+  }
+  res.status(400).send({ msg: 'Bad Request' })
 })
 
 router.put('/:id', (req, res) => {
-   const updatedPerson= updatePerson(req.params.id, req.body)
-   if(updatedPerson){
-        res.send(updatedPerson)
-   }
-   res.status(404).send({msg: 'Person Not Found'})
+  const updatedPerson = updatePerson(req.params.id, req.body)
+  if (updatedPerson) {
+    res.send(updatedPerson)
+  }
+  res.status(404).send({ msg: 'Person Not Found' })
 })
 
 router.delete('/:id', (req, res) => {
-    const deleted = deletePerson(req.params.id)
-    if (deleted) {
-        res.send({ msg: `Person ${req.params.id} has been Deleted.` })
-    }
-    res.status(404).send({msg: 'Person Not Found'}) 
+  const deleted = deletePerson(req.params.id)
+  if (deleted) {
+    res.send({ msg: `Person ${req.params.id} has been Deleted.` })
+  }
+  res.status(404).send({ msg: 'Person Not Found' })
 })
 
-export default router 
+export default router
